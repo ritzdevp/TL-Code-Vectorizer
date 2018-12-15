346ec112539a37ea278b6be35ecaa4457116056e
Rituraj Singh <rtjsingh30@gmail.com>
2018-12-15 15:16:54 +0530

    Update logistic_regression_ensembler.py

--- logistic_regression_ensembler.py
+++ logistic_regression_ensembler.py
@@ -7,6 +7,8 @@ Created on Sat Dec 15 14:08:40 2018
 
 import pandas as pd
 import performance_check as ensembler
+import preprocessingfile as preprocess
+from sklearn.metrics import *
 data = 'pc2.csv'
 original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val = preprocess.my_sdp_preprocessor(data)
 all_data = [original_data, original_X, original_Y,combined_training_data,x_train1,x_train2,x_train,x_test,x_val,y_train1,y_train2,y_train,y_test,y_val]
@@ -14,7 +16,7 @@ all_data = [original_data, original_X, original_Y,combined_training_data,x_train
 nn_clf, cnn_clf, svm_clf, rf_clf = ensembler.send_classifiers_to_LR_file()
 log_reg_clf, new_test_set_x_matrix = ensembler.send_results_to_logistic_regression()
 
-prediction = clf.predict(new_test_set_x_matrix)
+prediction = log_reg_clf.predict(new_test_set_x_matrix)
 print('Accuracy:',accuracy_score(y_test.values,prediction))
 
 
